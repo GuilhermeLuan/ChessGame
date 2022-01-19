5e34b131c1c687d1e0e25700d2fc0270541ee602
Guilherme Luan <guilhermeluan204@gmail.com>
2022-01-19 15:10:19 -0300

    feat: create class Position

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position position = new Position(10, 20);
+        System.out.println(position);
+    }
+}
