f971470fb33c48ec9adbd8e806c380e0ae8ae3b7
Guilherme Luan <guilhermeluan204@gmail.com>
2022-01-24 13:58:08 -0300

    feat: ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,12 +34,15 @@ public class ChessMatch {
         }
         return mat;
     }
+    private void placeNewPiece(char colomn, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(colomn, row).toPosition());
+    }
 
     private void initialSetup(){
         //Place the pieces on the board
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0 , 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7 , 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
 
     }
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -7,6 +7,10 @@ public class ChessPosition {
     private int row;
 
     public ChessPosition(char column, int row) {
+        //Error exception
+        if(column < 'a' || column > 'h' || row < 1 || row > 8){
+            throw new ChessException("Error instantiating ChessPositon. Valid values are from a1 to h8. ");
+        }
         this.column = column;
         this.row = row;
     }
@@ -15,15 +19,20 @@ public class ChessPosition {
         return column;
     }
 
-    public void setColumn(char column) {
-        this.column = column;
-    }
-
     public int getRow() {
         return row;
     }
 
-    public void setRow(int row) {
-        this.row = row;
+    //Conveter ChessPosition to positon
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+    //Converte Posotion to ChessPosition
+    protected static ChessPosition fromPostion(Position position){
+       return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+    @Override
+    public String toString(){
+        return "" + column + row;
     }
 }
