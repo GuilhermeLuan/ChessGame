e86a8a8fc8e526cdac79db454ac7eb8fe080b936
Guilherme Luan <guilhermeluan204@gmail.com>
2022-02-26 16:40:47 -0300

    feat: check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -24,6 +24,10 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public ChessPosition getChessPosition(){
+        return ChessPosition.fromPostion(position);
+    }
+
     public int getMoveCount() {
         return moveCount;
     }
